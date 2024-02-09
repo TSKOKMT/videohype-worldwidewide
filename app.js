939b3b7682e2c3022c6154507e46b25d41db059f
岡本斗志貴 <>
2024-02-09 17:30:41 +0900

    no message

--- app.js
+++ app.js
@@ -23,13 +23,11 @@ function generateHTML(dynamicText) {
         <style>
           @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
           @font-face {
-            font-family: "neo-sans";
             src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
             font-style: normal;
             font-weight: 700;
           }
           html {
-            font-family: neo-sans;
             font-weight: 700;
             font-size: calc(62rem / 16);
           }
@@ -56,12 +54,12 @@ function generateHTML(dynamicText) {
   `;
 }
 
-const IP = require('ip');
+//const IP = require('ip');
 
 // Route to serve the modified HTML content
 app.get("/", (req, res) => {
-  const clientIP = IP.address();
-  const modifiedHtml = generateHTML(clientIP);
+  //const clientIP = IP.address();
+  const modifiedHtml = generateHTML('Hello');
   res.type('html').send(modifiedHtml);
 });
 
