605ac3f9d60f4b83723f41a5068ac99e7d0c02ea
岡本斗志貴 <>
2024-04-29 23:16:37 +0900

    DOM bridge

--- app.js
+++ app.js
@@ -6,49 +6,20 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let clientCount = 0;
-let data = [];
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-//Connect
 io.on('connection', (socket) => {
-  console.log('A client connected');
-  clientCount++;
+    console.log('A user connected');
 
-  //Heartbeat
-  const heartbeatInterval = setInterval(() => {
-    if (!socket.connected) {
-        console.log('A client disconnected');
-        clientCount--;
-        clearInterval(heartbeatInterval);
-    }
-  }, 10000);
+    // 受け取ったHTMLを他のクライアントにブロードキャストする
+    socket.on('sendHtml', (html) => {
+        socket.broadcast.emit('htmlReceived', html);
+    });
 
-  //Disconnect
-  socket.on('disconnect', () => {
-    console.log('A client disconnected');
-    clientCount--;
-    clearInterval(heartbeatInterval);
-  });
-  
-  //Button pressed
-  socket.on('buttonPressed', (buttonData) => {
-    console.log('Received from client :', buttonData);
-    data.push([buttonData.text, buttonData.ip]);
-  });
-
-  //Update
-  socket.on('update', () => {
-    io.emit('data', data);
-    io.emit('clientCount', clientCount);
-  });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
-//Listen
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
